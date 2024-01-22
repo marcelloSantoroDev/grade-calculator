14389493e91274d0108cb9c078d084fa49459492
Marcello <marcelloaltermundi@gmail.com>
2024-01-22 14:38:48 -0300

    docs: adds comments for reqs #1 and #2

---
+++ src/main/java/com/trybe/java/regraprogressao/StudentRegister.java
@@ -0,0 +1,34 @@
+package com.trybe.java.regraprogressao;
+
+/**
+ * The type Student register.
+ */
+public class StudentRegister {
+
+  // instanciar scanner
+  // digite quantidade de atividade
+  // pegar a quantidade digitada e fazer um laço de repetição a partir desse número
+  // dentro desse laço, eu vou fazer mais perguntas através do scanner:
+  //
+  //    for (int i = 0; i < "n"; i++) {
+  //      "digite o nome da atividade" + i
+  //      "digite o peso da atividade" + i
+  //    }
+  //
+  //  deverá ser feito um array de atividades, um array de peso das atividades e um array de notas obtidas que se relacionarão
+  // através do seu índice (atividade 1, índice 0, peso da atividade 1, índice 0, nota da atividade 1, índice 0...)
+  // as atividades e seus respectivos pesos serão atribuídos dentro do laço de repetição
+  // ex.
+  //
+  //    for (int i = 0; i < "n"; i++) {
+  //      "digite o nome da atividade " + i
+  //      arrayDeAtividades[i] = Integer.parseInt(scanner.nextLine());
+  //      "digite o peso da atividade " + i
+  //      arrayDePesos[i] = Integer.parseInt(scanner.nextLine());
+  //      "digite a nota obtida para " + arraydeAtividades[i];
+  //      arrayDeNotas[i] = Integer.parseInt(scanner.nextLine());
+  //    }
+
+
+
+}
